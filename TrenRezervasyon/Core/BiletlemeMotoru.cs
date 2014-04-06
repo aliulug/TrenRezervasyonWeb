fc6163a53f85646503eebee572ec1ca7113422a4
aliulug <keremaliulug@gmail.com>
2014-04-07 02:59:17 +0300

    Vagonlara ait ayrıntılı testler eklendi

--- TrenRezervasyon/Core/BiletlemeMotoru.cs
+++ TrenRezervasyon/Core/BiletlemeMotoru.cs
@@ -2,20 +2,59 @@
 {
 	public class BiletlemeMotoru : IBiletlemeMotoru
 	{
+		private Tren _tren;
+		private RezervasyonIstegi _istek;
 		private const double TrenIcinMaksimumDolulukOrani = 0.7;
+		private const double VagonIcinMaksimumDolulukOrani = 0.7;
 
 		public RezervasyonIstekSonucu RezervasyonIstegiIsle(Tren tren, RezervasyonIstegi istek)
 		{
-			if (yeniRezervasyonTrenKapasitesiniAsiyor(tren, istek))
-				return new RezervasyonIstekSonucu{Basarili = false};
-			return new RezervasyonIstekSonucu { Basarili = true, KisiSayisi = istek.KisiSayisi, TrenAdi = istek.TrenAdi, VagonNo = 1};
+			_tren = tren;
+			_istek = istek;
+			RezervasyonIstekSonucu basarisizSonuc = new RezervasyonIstekSonucu();
+			if (yeniRezervasyonTrenKapasitesiniAsiyor(istek))
+				return basarisizSonuc;
+			Vagon rezervasyonYapilacakVagon = rezervasyonYapilacakVagonBul();
+			if (rezervasyonYapilacakVagon == null)
+				return basarisizSonuc;
+			return new RezervasyonIstekSonucu { Basarili = true, VagonNo = rezervasyonYapilacakVagon.VagonNo, KisiSayisi = istek.KisiSayisi, TrenAdi = istek.TrenAdi};
 		}
 
-		private bool yeniRezervasyonTrenKapasitesiniAsiyor(Tren tren, RezervasyonIstegi istek)
+		private Vagon rezervasyonYapilacakVagonBul()
+		{
+			Vagon rahatYerOlanVagon = rahatYerOlanVagonBul();
+			if (rahatYerOlanVagon != null)
+				return rahatYerOlanVagon;
+			return yerOlanVagonBul();
+		}
+
+		private Vagon yerOlanVagonBul()
+		{
+			foreach (Vagon vagon in _tren.Vagonlar)
+			{
+				int vagonToplamKoltukAdedi = vagon.BosKoltukSayisi + vagon.DoluKoltukSayisi;
+				if (vagon.DoluKoltukSayisi + _istek.KisiSayisi <= vagonToplamKoltukAdedi)
+					return vagon;
+			}
+			return null;
+		}
+
+		private Vagon rahatYerOlanVagonBul()
+		{
+			foreach (Vagon vagon in _tren.Vagonlar)
+			{
+				int vagonToplamKoltukAdedi = vagon.BosKoltukSayisi + vagon.DoluKoltukSayisi;
+				if ((vagon.DoluKoltukSayisi + _istek.KisiSayisi) / (double)vagonToplamKoltukAdedi <= VagonIcinMaksimumDolulukOrani)
+					return vagon;
+			}
+			return null;
+		}
+
+		private bool yeniRezervasyonTrenKapasitesiniAsiyor(RezervasyonIstegi istek)
 		{
 			int trendekiToplamKoltukSayisi = 0;
 			int trendekiToplamDoluKoltukSayisi = 0;
-			foreach (Vagon vagon in tren.Vagonlar)
+			foreach (Vagon vagon in _tren.Vagonlar)
 			{
 				trendekiToplamKoltukSayisi += vagon.BosKoltukSayisi + vagon.DoluKoltukSayisi;
 				trendekiToplamDoluKoltukSayisi += vagon.DoluKoltukSayisi;
--- TrenRezervasyonTest/Core/BiletlemeMotoruTest.cs
+++ TrenRezervasyonTest/Core/BiletlemeMotoruTest.cs
@@ -1,5 +1,4 @@
 ﻿// ReSharper disable InconsistentNaming
-
 using TrenRezervasyon.Core;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -9,49 +8,74 @@ namespace TrenRezervasyonTest.Core
 	[TestFixture]
 	public class BiletlemeMotoruTest
 	{
+		private BiletlemeMotoru _biletlemeMotoru;
+
+		[SetUp]
+		public void TestIlkAyarlar()
+		{
+			_biletlemeMotoru = new BiletlemeMotoru();
+		}
+
 		[Test]
 		public void OnKisilikTrenBos_BirKisilikRezervasyonIsteniyor_RezervasyonBasarili()
 		{
-			BiletlemeMotoru biletlemeMotoru = new BiletlemeMotoru();
 			Tren tren = new Tren { Vagonlar = new List<Vagon> { new Vagon { BosKoltukSayisi = 10, DoluKoltukSayisi = 0 } } };
-			RezervasyonIstekSonucu sonuc = biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 1 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 1 });
 			Assert.That(sonuc.Basarili, Is.EqualTo(true));
 		}
 
 		[Test]
 		public void OnKisilikTrendeAltiKisiVar_BirKisilikRezervasyonIsteniyor_RezervasyonBasarili()
 		{
-			BiletlemeMotoru biletlemeMotoru = new BiletlemeMotoru();
 			Tren tren = new Tren { Vagonlar = new List<Vagon> { new Vagon { BosKoltukSayisi = 4, DoluKoltukSayisi = 6 } } };
-			RezervasyonIstekSonucu sonuc = biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 1 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 1 });
 			Assert.That(sonuc.Basarili, Is.EqualTo(true));
 		}
 
 		[Test]
 		public void OnKisilikTrendeYediKisiVar_BirKisilikRezervasyonIsteniyor_RezervasyonBasarisiz()
 		{
-			BiletlemeMotoru biletlemeMotoru = new BiletlemeMotoru();
 			Tren tren = new Tren { Vagonlar = new List<Vagon> { new Vagon { BosKoltukSayisi = 2, DoluKoltukSayisi = 7 } } };
-			RezervasyonIstekSonucu sonuc = biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 1 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 1 });
 			Assert.That(sonuc.Basarili, Is.EqualTo(false));
 		}
 
 		[Test]
-		public void YuzKisilikIkiVagonBiri70Biri60Dolu_OnKisilikRezervasyonısteniyor_RezervasyonBasarili()
+		public void YuzKisilikIkiVagonBiri70Biri60Dolu_OnKisilikRezervasyonIsteniyor_RezervasyonBasarili()
 		{
-			BiletlemeMotoru biletlemeMotoru = new BiletlemeMotoru();
 			Tren tren = new Tren { Vagonlar = new List<Vagon> { new Vagon { BosKoltukSayisi = 30, DoluKoltukSayisi = 70 }, new Vagon { BosKoltukSayisi = 40, DoluKoltukSayisi = 60 } } };
-			RezervasyonIstekSonucu sonuc = biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 10 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 10 });
 			Assert.That(sonuc.Basarili, Is.EqualTo(true));
 		}
 
 		[Test]
-		public void YuzKisilikIkiVagonBiri70Biri61Dolu_OnKisilikRezervasyonısteniyor_RezervasyonBasarisiz()
+		public void YuzKisilikIkiVagonBiri70Biri61Dolu_OnKisilikRezervasyonIsteniyor_RezervasyonBasarisiz()
 		{
-			BiletlemeMotoru biletlemeMotoru = new BiletlemeMotoru();
 			Tren tren = new Tren { Vagonlar = new List<Vagon> { new Vagon { BosKoltukSayisi = 30, DoluKoltukSayisi = 70 }, new Vagon { BosKoltukSayisi = 39, DoluKoltukSayisi = 61 } } };
-			RezervasyonIstekSonucu sonuc = biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 10 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 10 });
 			Assert.That(sonuc.Basarili, Is.EqualTo(false));
+		}
+
+		[Test]
+		public void YuzKisilikOnVagonVarHepsi50Dolu_60KisilikRezervasyonIsteniyor_RezervasyonBasarisiz()
+		{
+			Tren tren = new Tren();
+			for (int i = 0; i < 10; i++)
+				tren.Vagonlar.Add(new Vagon { BosKoltukSayisi = 50, DoluKoltukSayisi = 50 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 60 });
+			Assert.That(sonuc.Basarili, Is.EqualTo(false));
+		}
+
+		[Test]
+		public void YuzKisilikOnVagonVar9u65KisiDoluSonuncusu10KisiDolu_6KisilikRezervasyonIsteniyor_RezervasyonSonVagonaYapilir()
+		{
+			Tren tren = new Tren();
+			for (int i = 0; i < 9; i++)
+				tren.Vagonlar.Add(new Vagon { BosKoltukSayisi = 35, DoluKoltukSayisi = 65, VagonNo = (i + 1) });
+			tren.Vagonlar.Add(new Vagon { BosKoltukSayisi = 90, DoluKoltukSayisi = 10, VagonNo = 10 });
+			RezervasyonIstekSonucu sonuc = _biletlemeMotoru.RezervasyonIstegiIsle(tren, new RezervasyonIstegi { KisiSayisi = 6 });
+			Assert.That(sonuc.Basarili, Is.EqualTo(true));
+			Assert.That(sonuc.VagonNo, Is.EqualTo(10));
 		}
 	}
 }
